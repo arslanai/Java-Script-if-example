d896645518736595a9a6e964268c0ee24b9e0e0f
Arslan <muhammadarslanrajput12@gmail.com>
2024-03-12 19:37:37 -0700

    Javascript example

---
+++ app.js
@@ -0,0 +1,45 @@
+// let school=confirm("School is open?");
+// if (school){document.write("Haadi go to School")}
+// else{document.write("Enloy you Holiday")};
+
+// let maske=+prompt("Type your entery test maske?");
+// if (maske>=100){
+//     document.write("congratulations enroll for SMIT")
+// }
+// else(document.write("Yor are not enroll for SMIT Aplay to again"));
+
+// let maske=+prompt("Type your entery test maske?");
+// if (maske===100){
+//     document.write("congratulations enroll for SMIT")
+// }
+// else(document.write("Yor are not enroll for SMIT Aplay to again"));
+
+
+// let admission = prompt("Admission open for School ");
+// if (admission==="metroPolisschool"){
+//     document.write("Qucikiy Admission ")}
+// else if(admission === "AliManePublicSchool"){document.write("Qucikiy Admission")}
+// else if (admission==="gojraPublicSchool"){
+//     document.write("Qucikiy Admission")}
+// else(document.write("you can admission"));
+
+
+
+let score = +prompt("type your Entry Test score");
+let qulification = prompt("type your qulification");
+if (score===70 || score ===100 && qulification==="inter" || qulification==="matric") {
+    document.write("congratulations enroll for SMIT");
+}
+else(document.write("Yor are not enroll for SMIT Aplay to again"));
+
+
+
+// let footBallScore = +prompt("type your foot Ball Score");
+// let CricketScore = +prompt("type your Cricket Score");
+// let TableTaniseScore = +prompt("type your Table Tanise Score");
+
+// if(footBallScore===50 && CricketScore<=50 || TableTaniseScore===20){
+//     document.write("Congratulation you are Admission");
+// }
+// else(document.write("Yor are not enroll for SMIT Aplay to again"));
+
